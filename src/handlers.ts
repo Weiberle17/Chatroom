77de5627b283ed8eb4dbb2b0e025b922ed97b8f9
Weiberle17 <maxweiberle63@gmail.com>
2023-03-08 14:11:44 +0100

    Environment variable names changed

--- src/handlers.ts
+++ src/handlers.ts
@@ -3,10 +3,10 @@ import mariadb from 'mariadb'
 require('dotenv').config()
 
 const pool = mariadb.createPool({
-  host: process.env.MYSQL,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
   connectionLimit: 5,
 })
 
